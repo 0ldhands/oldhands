1e1108c282bca7072b2aecc505157f3a47479f71
0ldhands <imakevinp11@gmail.com>
2025-09-15 14:19:32 +0530

    'reach'

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/Oldhandzwebsite",
   plugins: [react(),tailwindcss()],
+  base:"/Oldhandzwebsite",
 })
